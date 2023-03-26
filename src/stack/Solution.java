0c05dea8f03fe846e2c68f2fab9beec9c47fd8dc
xijie <907725402@qq.com>
2023-03-26 20:51:06 +0800

    栈及其应用

---
+++ src/stack/Solution.java
@@ -0,0 +1,37 @@
+package stack;
+
+
+//LeetCode  第20题：有效的括号
+public class Solution {
+    public boolean isValid(String s) {
+        ArrayStack<Character> stack = new ArrayStack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            }else {
+                if (stack.isEmpty()){
+                    return false;
+                }
+
+                char topChar = stack.pop();
+                if (c == ')' && topChar != '('){
+                    return false;
+                }
+                if (c == ']' && topChar != '['){
+                    return false;
+                }
+                if (c == '}' && topChar != '{'){
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().isValid("()[]{}"));
+        System.out.println(new Solution().isValid("(])]"));
+    }
+}
