6e537a8be0e024fd72912fd27f8fa8d658572ab5
xijie <907725402@qq.com>
2023-03-26 17:13:53 +0800

    数组

---
+++ src/array/Student.java
@@ -0,0 +1,56 @@
+package array;
+
+public class Student implements Comparable<Student> {
+    private String name;
+    private int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    @Override
+    public int compareTo(Student another) {
+//        if (this.score < another.score) {
+//            return -1;
+//        } else if (this.score == another.score) {
+//            return 0;
+//        } else {
+//            return 1;
+//        }
+//        return this.score - another.score;
+        return another.score - this.score;
+    }
+
+    @Override
+    public boolean equals(Object student) {
+
+        if (this == student) {
+            return true;
+        }
+
+        if (student == null) {
+            return false;
+        }
+
+        if (this.getClass() != student.getClass()) {
+            return false;
+        }
+
+        Student another = (Student) student;
+        return this.name.toLowerCase().equals(another.name.toLowerCase());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Student(name: %s, score: %d)", name, score);
+    }
+
+    public static void main(String[] args) {
+        Array<Student> arr = new Array<>();
+        arr.addLast(new Student("Alice", 100));
+        arr.addLast(new Student("Bob", 66));
+        arr.addLast(new Student("Charlie", 88));
+        System.out.println(arr);
+    }
+}
