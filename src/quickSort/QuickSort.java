8a81b71f44cd7ca85a3fa8767f75d90c2362d84c
xijie <907725402@qq.com>
2023-04-12 22:47:50 +0800

    玩转快速排序

--- src/quickSort/QuickSort.java
+++ src/quickSort/QuickSort.java
@@ -88,6 +88,38 @@ public class QuickSort {
         arr[j] = t;
     }
 
+    public static <E extends Comparable<E>> void sort3ways(E[] arr) {
+        Random rnd = new Random();
+        sort3ways(arr, 0, arr.length - 1, rnd);
+    }
+
+    private static <E extends Comparable<E>> void sort3ways(E[] arr, int l, int r, Random rnd) {
+        if (l >= r) return;
+
+        // 生成 [l, r] 之间的随机索引
+        int p = l + rnd.nextInt(r - l + 1);
+        swap(arr, l, p);
+
+        // arr[l+1, lt] < v ; arr[lt+1, i-1) == v ; arr[gt,r] > v
+        int lt = l, gt = r + 1, i = l + 1;
+        while (i < gt) {
+            if (arr[i].compareTo(arr[l]) < 0) {
+                lt++;
+                swap(arr, i, lt);
+                i++;
+            } else if (arr[i].compareTo(arr[l]) > 0) {
+                gt--;
+                swap(arr, i, gt);
+            } else { // arr[i] == arr[l]
+                i++;
+            }
+        }
+        swap(arr, l, lt);
+        // arr[l+1, lt-1] < v ; arr[lt, gt-1] == v ; arr[gt,r] > v
+        sort3ways(arr, l, lt - 1, rnd);
+        sort3ways(arr, gt, r, rnd);
+    }
+
     public static void main(String[] args) {
 
         int n = 100000;
