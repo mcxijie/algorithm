11755eeedc79c058eea35c9656e20b23ac07d898
xijie <907725402@qq.com>
2023-04-22 18:15:21 +0800

    二分搜索树删除问题

--- src/BST/Main.java
+++ src/BST/Main.java
@@ -1,31 +1,59 @@
 package BST;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
 
+//    public static void main(String[] args) {
+//        BST bst = new BST();
+//        int[] nums = {5, 3, 6, 8, 4, 2};
+//        for (int num : nums) {
+//            bst.add(num);
+//        }
+//        /////////////////
+//        //      5      //
+//        //    /   \    //
+//        //   3    6    //
+//        //  / \    \   //
+//        // 2  4     8  //
+//        /////////////////
+//
+//        bst.preOrder();
+//        System.out.println();
+//
+//        bst.inOrder();
+//        System.out.println();
+//
+//        bst.postOrder();
+//        System.out.println();
+//
+////        System.out.println(bst);
+//    }
+
     public static void main(String[] args) {
-        BST bst = new BST();
-        int[] nums = {5, 3, 6, 8, 4, 2};
-        for (int num : nums) {
-            bst.add(num);
+        BST bst = new BST<>();
+        Random random = new Random();
+
+        int n = 1000;
+        for (int i = 0; i < n; i++) {
+            bst.add(random.nextInt(10000));
         }
-        /////////////////
-        //      5      //
-        //    /   \    //
-        //   3    6    //
-        //  / \    \   //
-        // 2  4     8  //
-        /////////////////
 
-        bst.preOrder();
-        System.out.println();
+        ArrayList<Integer> nums = new ArrayList<>();
+        while (!bst.isEmpty()) {
+            nums.add((Integer) bst.removeMin());
+        }
 
-        bst.inOrder();
-        System.out.println();
+        System.out.println(nums);
 
-        bst.postOrder();
-        System.out.println();
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums.get(i - 1) > nums.get(i)) {
+                throw new IllegalArgumentException("Error");
+            }
+        }
 
-//        System.out.println(bst);
+        System.out.println("removeMin test completed.");
     }
 
 
